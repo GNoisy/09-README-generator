2aa6f38fedcfed2d88b192681f4477a632078f67
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-10 20:36:18 -0700

    updated questions

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ const inquirer = require("inquirer");
 
 const questions = [
     {
-      type: 'input',
-      name: 'title',
-      message: 'Enter the project title: ',
+      type: "input",
+      name: "title",
+      message: "Enter the project title: ",
     },
     {
-      type: 'list',
-      name: 'license',
-      choices: ['MIT', 'GPL', 'Apache', 'BSD', 'None'],
-      message: 'Select the project license: ',
+      type: "input",
+      name: "description",
+      message: "Describe your project and motive behind building it: ",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the steps required to install your project: ',
+      type: "input",
+      name: "installation",
+      message: "What steps are required to install your project: ",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide instructions and examples for use.: ',
+      type: "input",
+      name: "usage",
+      message: "Provide steps to install your project: ",
     },
-];
+    {
+      type: "input",
+      name: "credits",
+      message: "Submit names, links or any required attribution, if any, here: ",
+    },
+    {
+      type: "list",
+      name: "license",
+      choices: ["MIT", "GPL", "Apache", "BSD", "None"],
+      message: "Select the project license: ",
+    },
+  ];
